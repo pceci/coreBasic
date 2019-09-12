7c8fdeb2e0486aad3c3a6c76bf2a9493cc251673
CeciP <CeciP@sancristobal.com.ar>
2019-09-12 16:23:28 -0300

    [DONE] Api reglas-rol

--- API/ReglasRolApiController.cs
+++ API/ReglasRolApiController.cs
@@ -1,67 +1,38 @@
-using Microsoft.AspNetCore.Mvc;
-using coreBasic.Entities;
 using System.Linq;
 using coreBasic.Business;
+using coreBasic.Entities;
+using Microsoft.AspNetCore.Mvc;
 
-namespace netCoreWorkshop.API
-{
-    [Route("/api/reglas")]
+namespace netCoreWorkshop.API {
+    [Route ("/api/reglas")]
     [ApiController]
-    public class ReglasApiController : ControllerBase
-    {
+    public class ReglasApiController : ControllerBase {
         private readonly IAdminService adminService;
 
-        public ReglasApiController(IAdminService adminService)
-        {
+        public ReglasApiController (IAdminService adminService) {
             this.adminService = adminService;
         }
-        [HttpGet("{id}")]
-        public IActionResult Get(int id)
-        {
-            var obj = adminService.GetAllReglasRol(id);
 
-            if (obj == null)
-            {
-                return NotFound();
+        [HttpGet ("{id}")]
+        public IActionResult Get (int id) {
+            var obj = adminService.GetAllReglasRol (id);
+
+            if (obj == null) {
+                return NotFound ();
             }
 
-            return Ok(obj);
+            return Ok (obj);
         }
-        [HttpPost]
-        public IActionResult Create([FromBody]List<cReglaPorRol> reglasRol)
-        {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
+        [HttpPut ("{id}")]
+        public IActionResult Edit (int id, [FromBody] List<cReglaPorRol> reglasRol) {
+            if (!ModelState.IsValid) {
+                return BadRequest (ModelState);
             }
-
-            regla = adminService.EditReglasRol(regla);
-
-            return CreatedAtAction(nameof(Create), new { id = regla.id }, regla);
-        }
-
-        [HttpPut("{id}")]
-        public IActionResult Edit(int id, [FromBody]cRegla regla)
-        {
-
-            if (id != regla.id)
-            {
-                return NotFound();
+            var current = adminService.EditReglasRol (id, reglasRol);
+            if (current == null) {
+                return NotFound ();
             }
-
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
-
-            var current = adminService.EditRegla(id, regla);
-
-            if (current == null)
-            {
-                return NotFound();
-            }
-
-            return NoContent();
+            return NoContent ();
         }
 
     }
